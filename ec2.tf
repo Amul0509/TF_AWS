830978e9378232c7b9be185770362e814a3ed1a9
Amul0509 <124345993+Amul0509@users.noreply.github.com>
2023-09-03 22:34:45 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -109,7 +109,7 @@ resource "null_resource" "name" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("/var/lib/jenkins/workspace/test/IAMKeypair")
+    private_key = file("/var/lib/jenkins/workspace/test/AWSKeypair")
     host        = aws_instance.ec2_instance.public_ip
   }
 
